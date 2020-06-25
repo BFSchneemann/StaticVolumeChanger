36a791b72b3a8c8593ed84c43d3243f1c806548d
Chris <Christian_1997@outlook.de>
2020-06-25 13:18:46 +0200

    version 1.0

---
+++ MicController/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace MicController
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblDeviceName = new System.Windows.Forms.Label();
+            this.lblCurrVolume = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtVolume = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblChangeCounter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDeviceName
+            // 
+            this.lblDeviceName.AutoSize = true;
+            this.lblDeviceName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDeviceName.Location = new System.Drawing.Point(24, 43);
+            this.lblDeviceName.Name = "lblDeviceName";
+            this.lblDeviceName.Size = new System.Drawing.Size(21, 20);
+            this.lblDeviceName.TabIndex = 2;
+            this.lblDeviceName.Text = "...";
+            // 
+            // lblCurrVolume
+            // 
+            this.lblCurrVolume.AutoSize = true;
+            this.lblCurrVolume.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrVolume.Location = new System.Drawing.Point(24, 106);
+            this.lblCurrVolume.Name = "lblCurrVolume";
+            this.lblCurrVolume.Size = new System.Drawing.Size(21, 20);
+            this.lblCurrVolume.TabIndex = 3;
+            this.lblCurrVolume.Text = "...";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Gerätename:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "aktueller Wert:";
+            // 
+            // txtVolume
+            // 
+            this.txtVolume.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtVolume.Location = new System.Drawing.Point(286, 105);
+            this.txtVolume.MaxLength = 3;
+            this.txtVolume.Name = "txtVolume";
+            this.txtVolume.Size = new System.Drawing.Size(100, 21);
+            this.txtVolume.TabIndex = 6;
+            this.txtVolume.Text = "0.8";
+            this.txtVolume.TextChanged += new System.EventHandler(this.TxtVolume_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(282, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Vorgabe:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(24, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(174, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "versuchte Änderungen:";
+            // 
+            // lblChangeCounter
+            // 
+            this.lblChangeCounter.AutoSize = true;
+            this.lblChangeCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChangeCounter.Location = new System.Drawing.Point(24, 161);
+            this.lblChangeCounter.Name = "lblChangeCounter";
+            this.lblChangeCounter.Size = new System.Drawing.Size(21, 20);
+            this.lblChangeCounter.TabIndex = 9;
+            this.lblChangeCounter.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 190);
+            this.Controls.Add(this.lblChangeCounter);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtVolume);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblCurrVolume);
+            this.Controls.Add(this.lblDeviceName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Microphone Controller";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblDeviceName;
+        private System.Windows.Forms.Label lblCurrVolume;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtVolume;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblChangeCounter;
+    }
+}
+
---
+++ MicController/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using NAudio.CoreAudioApi;
+
+namespace MicController
+{
+    public partial class Form1 : Form
+    {
+        private MMDevice defaultDevice;
+        private static System.Timers.Timer timer;
+        private Int32 globalChangeCounter = 0;
+        private static NotifyIcon notifyIcon;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Create_KontextMenu();
+
+            var enumerator = new MMDeviceEnumerator();
+            defaultDevice = enumerator
+                .GetDefaultAudioEndpoint(
+                DataFlow.Capture,
+                Role.Communications);
+
+            lblDeviceName.Text = defaultDevice.FriendlyName;
+            lblCurrVolume.Text = defaultDevice.AudioEndpointVolume.MasterVolumeLevel.ToString();
+
+            timer = new System.Timers.Timer(50);
+            timer.AutoReset = true;
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Create_KontextMenu()
+        {
+            ContextMenu cm = new ContextMenu();
+            MenuItem miCurr;
+            int iIndex = 0;
+
+            // Kontextmenüeinträge erzeugen
+            miCurr = new MenuItem();
+            miCurr.Index = iIndex++;
+            miCurr.Text = "&Öffnen";
+            miCurr.Click += new EventHandler(Show_Window);
+            cm.MenuItems.Add(miCurr);
+
+            // Kontextmenüeinträge erzeugen
+            miCurr = new MenuItem();
+            miCurr.Index = iIndex++;
+            miCurr.Text = "&Beenden";
+            miCurr.Click += new EventHandler(Exit_Clicked);
+            cm.MenuItems.Add(miCurr);
+
+            // NotifyIcon selbst erzeugen
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = Properties.Resources.mic; // Eigenes Icon einsetzen
+            //notifyIcon.Text = "Doppelklick mich!";   // Eigenen Text einsetzen
+            notifyIcon.Visible = true;
+            notifyIcon.ContextMenu = cm;
+            notifyIcon.DoubleClick += new EventHandler(Show_Window);
+        }
+
+        private void Show_Window(object sender, System.EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+        }
+
+        private void Exit_Clicked(object sender, System.EventArgs e)
+        {
+            notifyIcon.Dispose();
+            Application.Exit();
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (lblCurrVolume.InvokeRequired)
+            {
+                lblCurrVolume.Invoke((MethodInvoker)delegate ()
+                {
+                    Manipulate_Timer();
+                }
+                );
+            }
+            else
+            {
+                Manipulate_Timer();
+            }
+            
+        }
+
+        private void Manipulate_Timer()
+        {
+            float oldVol = defaultDevice.AudioEndpointVolume.MasterVolumeLevelScalar;
+            float newVol = float.Parse(txtVolume.Text, System.Globalization.CultureInfo.InvariantCulture);
+
+            lblCurrVolume.Text = oldVol.ToString();
+
+            defaultDevice.AudioEndpointVolume.MasterVolumeLevelScalar = newVol;
+
+            if (oldVol != newVol)
+            {
+                globalChangeCounter++;
+                lblChangeCounter.Text = globalChangeCounter.ToString();
+            }
+        }
+
+        private void TxtVolume_TextChanged(object sender, EventArgs e)
+        {
+            globalChangeCounter = -1;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.ShowInTaskbar = false;
+            }
+        }
+    }
+}
